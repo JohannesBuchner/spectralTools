66a1b0b87886cfe8e1aeb62893663af9fa0792bf
J. Michael Burgess <jmichaelburgess@gmail.com>
2015-02-06 16:23:08 -0500

    added a whole bunch of sig test

--- variability/variFilter.py
+++ variability/variFilter.py
@@ -4,8 +4,8 @@ from scipy.stats import pearsonr
 from spectralTools.lightCurve_data import lightCurve_data as LCD
 
 
-from numpy import logspace, array, log10
-
+from numpy import logspace, array, log10, sqrt, r_, zeros, arange, log, log10
+from numpy.random import normal
 
 class variFilter(object):
 
@@ -48,7 +48,7 @@ class variFilter(object):
         self._time = lc.GetTime()
 
 
-    def CalcVari(self,recalc=False):
+    def CalcVari(self,recalc=False,ret=True):
 
 
         if recalc:
@@ -69,7 +69,7 @@ class variFilter(object):
 
         RLC = []
         for cutoff in fGrid:
-            y = self._butter_filter(cutoff)
+            y = self._butter_filter(cutoff,self._counts)
             RLC.append(y)
     
 
@@ -81,9 +81,38 @@ class variFilter(object):
 
 
 
+
+        
+        R=array(R)
+
+        test = r_[True, R[1:] < R[:-1]] & r_[R[:-1] < R[1:], True]    
+        test[0]=False
+        test[-1]=False
+        
+        indx = arange(len(R))[test]
+
+        Sn = []
+        for i in indx:
+
+            sn = min( (R[i-1] - R[i]) /(log(fGrid[i]) - log(fGrid[i-1]) )   ,  (R[i+1] - R[i]) /(log(fGrid[i+1]) - log(fGrid[i]) ) )
+            Sn.append(sn)
+        Sn=array(Sn)/max(Sn)
+
+
+        self.Sn = Sn
+        self.dips = test
+            
         self.RLC = array(RLC)
 
-        return [fGrid[:-1],array(R)]
+        self.R = array(R)
+        self.fGrid = fGrid[:-1]
+
+
+
+        
+
+        if ret:
+            return [fGrid[:-1],array(R)]
 
 
     def _butter_coeff(self,cutoff):
@@ -92,12 +121,87 @@ class variFilter(object):
         b, a = butter(self.order, normal_cutoff, btype=self.fType, analog=False)
         return b, a
 
-    def _butter_filter(self,cutoff):
+    def _butter_filter(self,cutoff,data):
         b, a = self._butter_coeff(cutoff)
-        y = lfilter(b, a, self._counts)
+        y = lfilter(b, a, data)
         return y
 
 
+    def _SimLightCurve(self):
+
+        Nsim = 1000 #number of sim lightcurves
+
+        simLC = []
+
+        for i in range(Nsim):
+            
+            simLC.append(map(self._genCount,self._counts))
+
+        simLC = array(simLC)
 
 
+        fGrid = logspace(log10(self.fmin),log10(self.fmax),100)
+        fHist = zeros(len(fGrid))
+        for slc in simLC:
+
+            RLC = []
+            for cutoff in fGrid:
+                y = self._butter_filter(cutoff,slc)
+                RLC.append(y)
+    
+
+            R=[]
+            for i in range(len(RLC)-1):
+    
+                r = pearsonr(RLC[i],RLC[i+1])[0]
+                R.append(r)
+
+            R=array(R)
+            test = r_[True, R[1:] < R[:-1]] & r_[R[:-1] < R[1:], True]
+            test[0] = False
+            test[-1] = False
+            fHist[test]+=1.
+
+        self.fHist=fHist/Nsim
+    
+        
+    def _genCount(self,x):
+
+
+        if x == 0.:
+
+            return 0.
+
+
+        else:
+
+            return normal(x,sqrt(x))
+
+
+
+    def GetSignificance(self):
+
+
+        return self.fHist[self.dips]
+
+
+    def GetFreq(self):
+
+
+        return self.fGrid[self.dips]
+
+    def GetR(self):
+
+        return self.R[self.dips]
+
+    def GetRLC(self):
+
+        return self.RLC[self.dips]
+
+
+
+
+
+        
+     
     
